bb5ad9ec33da022659cde41095a72ce5f90a9f7f
Yardley Francois <144744560+YardleyFra@users.noreply.github.com>
2024-04-01 15:59:51 -0400

    professorHelp

--- calculatorApp/test/test_calculator.py
+++ calculatorApp/test/test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from calculatorApp.app.calculator import add, subtract, multiply, divide
+from app.calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
